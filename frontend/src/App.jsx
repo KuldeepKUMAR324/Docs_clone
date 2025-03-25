bc80eb9fcc914f86a25eb692db0f96f193363fb6
kuldeep sharma <kuldeep.kumar_cs22@gla.ac.in>
2025-03-25 09:51:09 +0530

    initial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,26 +8,40 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex'>
+        <div><img src="https://storage.googleapis.com/gweb-workspace-assets/uploads/7uffzv9dk4sn-7kAnuMn2JYM8SouH9sUO1Y-5decaa2561233114d97a43fceaa78f4f-Docs_Full_Logo.svg" alt="not show" />
+
+          <p className='text-5xl mt-4 ml-14 font-bold' >online</p>
+          <p className='text-5xl mt-3 ml-14 font-bold'>collaborative</p>
+          <p className='text-5xl mt-3 ml-14 font-bold'>Documents</p>
+          <p className='mt-3 '>AI-powered documents to help you and your team create and </p>
+          <p>   collaborate on content.</p>
+          
+
+          <button type="button" class="focus:outline-none text-white ml-24 mt-10  bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">SignUp</button>
+        </div>
+
+
+
+        <div className=' '>
+          <img src="https://lh3.googleusercontent.com/Z46v0WUwJTOMu4_KY4Z46AFSfo5C4z3UdTMOsbHKDI4tRRsK3fElJVKzmUapYydds5tifA-XXelhBaNtI0x9Un4DnyN2VPlu_nQv8A=e365-pa-nu-rw-w1416" className='h-110' alt="" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </>
   )
 }
